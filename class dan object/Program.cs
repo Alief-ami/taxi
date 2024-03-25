83ef7ac65125e8528326edca5952018b0c8053be
Alief <aliefelfaqihyulizar@gmail.com>
2024-03-25 22:22:16 +0700

    Add project files.

---
+++ class dan object/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_dan_object
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            taxi Taxi = new taxi();
+            Taxi.Name = "budi";
+            Taxi.OnDuty = true;
+            Taxi.Id = 10;
+
+            Taxi.orderan();
+            Taxi.menjemput();
+            Taxi.mengantar();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ class dan object/taxi.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_dan_object
+{
+    internal class taxi
+    {
+        public string Name { get; set; }
+        public bool OnDuty { get; set; }
+        public float Id { get; set; }
+
+
+        public void orderan()
+        {
+            Console.WriteLine("Driver Name: {0}", Name);
+            Console.WriteLine("On Duty: {0}", OnDuty);
+            Console.WriteLine("Number of Passenger: {0}\n", Id);
+        }
+
+        public void menjemput()
+        {
+            Console.WriteLine("rafi sedang menjemput penumpang", Name);
+        }
+
+        public void mengantar()
+        {
+            Console.WriteLine("rafi selesai mengantar penumpang", Name);
+        }
+    }
+}
+
